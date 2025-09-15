b0a5d46cf0714daee12d3f20ebe1e513af324cb0
Ryan Nguyen <datdevsx@gmail.com>
2025-09-15 17:51:01 +0700

    Update Firebase configuration (#1)

--- apps/admin-panel/src/config/firebase.ts
+++ apps/admin-panel/src/config/firebase.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: '',
-  appId: '',
-  authDomain: '',
-  measurementId: '',
-  messagingSenderId: '',
-  projectId: '',
-  storageBucket: '',
+  apiKey: 'YOUR_GOOGLE_API_KEY_HERE',
+  appId: '1:673833772088:web:6d7eaf7262a4ebc87372cd',
+  authDomain: 'imdat-dev.firebaseapp.com',
+  measurementId: 'G-0BCZHM0BDZ',
+  messagingSenderId: '673833772088',
+  projectId: 'imdat-dev',
+  storageBucket: 'imdat-dev.appspot.com',
 };
