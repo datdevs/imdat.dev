465bcae01ebb6e4ffacde46e652154eb64915aed
datdevs <datdevsx@gmail.com>
2025-10-15 17:56:35 +0700

    feat: Implement dashboard layout and data visualization components
    - Created a new dashboard layout with sections for total, published, draft, and archived portfolios.
    - Added pie and bar charts for status and technology distribution using TaigaUI components.
    - Integrated recent portfolios display with loading states and edit functionality.
    - Updated styles for dashboard components using Tailwind CSS.

--- apps/admin-panel/src/app/models/common/pagination.ts
+++ apps/admin-panel/src/app/models/common/pagination.ts
@@ -2,9 +2,9 @@ export interface Pagination<T> {
   cursor?: 'next' | 'prev';
   firstDoc?: T;
   lastDoc?: T;
-  limit: number;
+  limit?: number;
   orderBy: keyof T;
   orderDirection: 'asc' | 'desc';
-  page: number; // 1-based page number
+  page?: number; // 1-based page number
   total?: number;
 }
--- apps/admin-panel/src/app/pages/dashboard/dashboard.ts
+++ apps/admin-panel/src/app/pages/dashboard/dashboard.ts
@@ -1,10 +1,93 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DatePipe, NgOptimizedImage } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, inject, Signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TuiAxes, TuiBarChart, TuiLegendItem, TuiPieChart } from '@taiga-ui/addon-charts';
+import { TuiHovered } from '@taiga-ui/cdk/directives/hovered';
+import { TuiAppearance, TuiButton } from '@taiga-ui/core';
+import { TuiAvatar, TuiSkeleton } from '@taiga-ui/kit';
+import { TuiCard, TuiCell, TuiHeader } from '@taiga-ui/layout';
+
+import { StatusEnum } from '../../core/constants/status';
+import { IPortfolio } from '../../models/portfolio';
+import { PortfolioStore } from '../../store/portfolio/portfolio.store';
+import { StatusPipe } from '../../utils/pipes';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    RouterLink,
+    DatePipe,
+    StatusPipe,
+    NgOptimizedImage,
+
+    TuiHeader,
+    TuiAvatar,
+    TuiCell,
+    TuiButton,
+    TuiCard,
+    TuiAxes,
+    TuiBarChart,
+    TuiPieChart,
+    TuiLegendItem,
+    TuiAppearance,
+    TuiHovered,
+    TuiSkeleton,
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Dashboard {}
+export class Dashboard {
+  private readonly portfolioStore = inject(PortfolioStore);
+  protected readonly isDashboardLoading: Signal<boolean> = this.portfolioStore.isDashboardLoading;
+  protected readonly isRecentPortfoliosLoading: Signal<boolean> = this.portfolioStore.isRecentPortfoliosLoading;
+
+  protected readonly recentPortfolios: Signal<IPortfolio[]> = computed(() => this.portfolioStore.recentPortfolios());
+  protected readonly dashboardPortfolios: Signal<{
+    archived: number;
+    draft: number;
+    published: number;
+    statusDistribution: { count: number; status: StatusEnum }[];
+    statusDistributionValue: number[];
+    total: number;
+  }> = computed(() => {
+    const portfolios = this.portfolioStore.dashboardPortfolios();
+    const published = portfolios?.filter((p) => p.status === StatusEnum.PUBLISHED).length ?? 0;
+    const draft = portfolios?.filter((p) => p.status === StatusEnum.DRAFT).length ?? 0;
+    const archived = portfolios?.filter((p) => p.status === StatusEnum.ARCHIVED).length ?? 0;
+
+    const statusDistribution = [
+      { status: StatusEnum.PUBLISHED, count: published },
+      { status: StatusEnum.DRAFT, count: draft },
+      { status: StatusEnum.ARCHIVED, count: archived },
+    ];
+
+    return {
+      published,
+      draft,
+      archived,
+      total: portfolios.length,
+      statusDistribution,
+      statusDistributionValue: statusDistribution.map((s) => s.count),
+    };
+  });
+
+  protected activeItemIndex = NaN;
+
+  protected readonly value = [13769, 12367, 10172, 3018, 2592];
+
+  protected readonly labels = ['Food', 'Cafe', 'OSS', 'Taxi', 'Other'];
+
+  constructor() {
+    this.portfolioStore.loadDashboardPortfolios();
+    this.portfolioStore.loadRecentPortfolios();
+  }
+
+  protected isItemActive(index: number): boolean {
+    return this.activeItemIndex === index;
+  }
+
+  protected onHover(index: number, hovered: any): void {
+    this.activeItemIndex = hovered ? index : NaN;
+  }
+}
