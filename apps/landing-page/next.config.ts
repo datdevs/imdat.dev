35d616473d219054c9f43cd2476dee845972f65a
datdevs <datdevsx@gmail.com>
2025-10-27 00:07:52 +0700

    chore: Clean up CI workflow and add Next.js configuration for landing page

--- apps/landing-page/next.config.ts
+++ apps/landing-page/next.config.ts
@@ -10,14 +10,9 @@ const { join } = require('path');
 const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
-  nx: {},
-  output: 'standalone',
-  outputFileTracingRoot: join(__dirname, '../../'),
+  // nx: {},
+  // output: 'standalone',
+  // outputFileTracingRoot: join(__dirname, '../../'),
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
-
-module.exports = composePlugins(...plugins)(nextConfig);
+module.exports = composePlugins()(nextConfig);
