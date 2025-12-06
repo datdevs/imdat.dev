8ef40e0d49a27ad1efaf391d0c968bace5f688e1
datdevs <datdevsx@gmail.com>
2025-12-06 11:37:47 +0700

    feat: Enhance Next.js configuration with image support and improve CSS animations

--- apps/wedding/next.config.js
+++ apps/wedding/next.config.js
@@ -8,11 +8,21 @@ const { join } = require('node:path');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'storage.imdat.dev',
+        protocol: 'https',
+      },
+    ],
+  },
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   // nx: {},
   output: 'standalone',
   outputFileTracingRoot: join(__dirname, '../../'),
+  poweredByHeader: false,
+  reactStrictMode: true,
 };
 
 module.exports = composePlugins()(nextConfig);
