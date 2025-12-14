b4a734a4b5526d69bb4c26f3189b8f08f8c33a83
datdevs <datdevsx@gmail.com>
2025-12-14 16:28:06 +0700

    fix: enhance locale handling in proxy and update matcher to include assets

--- apps/wedding/src/proxy.ts
+++ apps/wedding/src/proxy.ts
@@ -6,6 +6,7 @@ import { i18n } from './core/i18n/config';
 
 export function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
+
   const pathnameIsMissingLocale = i18n.locales.every(
     (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
   );
@@ -16,9 +17,11 @@ export function proxy(request: NextRequest) {
     const newUrl = new URL(`/${locale}${pathname}`, request.url);
     return NextResponse.redirect(newUrl);
   }
+
+  return NextResponse.next();
 }
 
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|assets).*)'],
 };
